67c89efb2dca0cf0802fcd37ee7ce8e162b9cbe2
Pavel Tížek <pavel@tizek.cz>
2019-07-19 10:08:01 +0200

    Added Heureka Availability

---
+++ src/Generators/HeurekaAvailability/Depot.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Mk\Feed\Generators\Heureka;
+
+
+use Mk, Nette;
+
+/**
+ * Class Image
+ * @author Martin Knor <martin.knor@gmail.com>
+ * @package Mk\Feed\Generators\Heureka
+ */
+class Depot
+{
+    /* Použití smartobject viz php 7.2 to nette 2.4 */
+    use \Nette\SmartObject;
+
+
+
+    /** @var string */
+    private $id;
+    /**
+     * @var int
+     */
+    private $stock;
+
+    /**
+     * Image constructor.
+     * @param string $url
+     */
+    public function __construct($id, int $stock)
+    {
+        $this->id = $id;
+        $this->stock = $stock;
+    }
+
+    /**
+     * @return string
+     */
+    public function getId(): string {
+        return $this->id;
+    }
+
+    /**
+     * @return int
+     */
+    public function getStock(): int {
+        return $this->stock;
+    }
+
+
+
+
+
+}
